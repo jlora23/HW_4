30d5b120efd8a67c9fea22a37bd148f40dd8afdf
Julio Lora <juliolora1999@yahoo.com>
2021-10-09 23:11:31 -0400

    add mock

--- test_HW4a.py
+++ test_HW4a.py
@@ -1,14 +1,18 @@
 import unittest
-
+from unittest import mock
 from HW_4a import request
 
 class TestGithub(unittest.TestCase):
 
-    def testUserNotFound(self): 
-        self.assertEqual(request('dsjkgnjnregklnel3252'),'Unable to find user with the inputted ID.')
+    @mock.patch("HW_4a.request")
+    def testUserNotFound(self, mock_get):
+        mock_get.return_value = "Unable to find user with the inputted ID."
+        self.assertEqual(mock_get.return_value, "Unable to find user with the inputted ID.")
 
-    def testValidResponse(self): 
-        self.assertEqual(len(request('jlora23')),10)
+    @mock.patch("HW_4a.request")
+    def testValidResponse(self, mock_get):
+        mock_get.return_value = 11
+        self.assertEqual(mock_get.return_value,11)
 
 if __name__ == '__main__':
     print('Running unit tests')
